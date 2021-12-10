fb7991d667d551d5d555d84c2f9c2729f2a45baf
Hover21 <hoverdurte@gmail.com>
2021-12-10 12:10:46 -0500

    Validación Elefante X

--- src/ZafariKids.java
+++ src/ZafariKids.java
@@ -346,7 +346,8 @@ public class ZafariKids extends javax.swing.JFrame {
 
     private void jButtonCheckActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCheckActionPerformed
         if (jlblAguila.getX() >= 1030 && jlblAguila.getX() <= 1037 &&jlblAguila.getY() >= 105 &&  + jlblAguila.getY() <= 115 
-                && jlblCebra.getX()>= 795 && jlblCebra.getX()<= 800 && jlblCebra.getY() >= 295  &&  + jlblCebra.getY() <= 300 ) 
+                && jlblCebra.getX()>= 795 && jlblCebra.getX()<= 800 && jlblCebra.getY() >= 295  &&  + jlblCebra.getY() <= 300 
+                && jlblElefante.getX()>= 715 && + jlblElefante.getX()<= 725 && + jlblElefante.getY()>= 545 && + jlblElefante.getY()<= 550) 
         {JOptionPane.showMessageDialog(this, "PRUEBA");
             
         } else {
